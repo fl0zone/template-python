41ddf9e940300fa1498656ab475b8df9725c2d54
James Harrison <james@fl0.com>
2023-05-24 13:14:49 -0700

    Added pretty static page

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory, render_template, redirect
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello from FL0!</p>"
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/<path:path>')
+def all_routes(path):
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(port=port)
